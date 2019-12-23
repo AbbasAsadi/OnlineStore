25af930d4e8e82d1781075dc1fd7654bd2867ba6
abbas asadi <abbas.asadi1996@gmail.com>
2019-12-23 18:44:36 +0330

    add navigationDrawer and navigationView

--- app/src/main/java/com/example/onlinestore/MainActivity.java
+++ app/src/main/java/com/example/onlinestore/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.onlinestore;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends SingleFragmentActivity {
+
+public class MainActivity extends SingleFragmentActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Toolbar mTopToolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
 
     @Override
@@ -23,16 +31,17 @@ public class MainActivity extends SingleFragmentActivity {
 
     @Override
     public int getResourceId() {
-        return R.id.activity_main;
+        return R.id.container;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTopToolbar = findViewById(R.id.my_toolbar);
-        mTopToolbar.setTitle(" ");
-        setSupportActionBar(mTopToolbar);
+
+        this.configureToolBar();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
     }
 
     @Override
@@ -40,4 +49,62 @@ public class MainActivity extends SingleFragmentActivity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public void onBackPressed() {
+        // Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // Handle Navigation Item Click
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.activity_main_drawer_news:
+                break;
+            case R.id.activity_main_drawer_profile:
+                break;
+            case R.id.activity_main_drawer_settings:
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    // Configure Toolbar
+    private void configureToolBar() {
+        this.mTopToolbar = findViewById(R.id.my_toolbar);
+        mTopToolbar.setTitle(" ");
+        setSupportActionBar(mTopToolbar);
+    }
+
+    // Configure Drawer Layout
+    private void configureDrawerLayout() {
+        this.drawerLayout = findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, mTopToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // Configure NavigationView
+    private void configureNavigationView() {
+        this.navigationView =  findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
 }
